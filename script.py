6c26d39679a3fc14f58ef2aba2e49cd0aebd5230
Veronika Babii <57755903+VeronikaBabii@users.noreply.github.com>
2022-02-06 20:46:36 +0200

    Update script.py

--- script.py
+++ script.py
@@ -6,32 +6,27 @@ import matplotlib.cm as plt
 print("1 - Load a ply point cloud and visualize it")
 pcd = o3d.io.read_point_cloud("/Users/veronika/Desktop/input_data.ply")
 o3d.visualization.draw_geometries([pcd])
-print("Done 1")
 
 # downsample
 print("2 - Downsample the point cloud with a voxel of 0.01")
-downpcd = pcd.voxel_down_sample(voxel_size=0.02)
+downpcd = pcd.voxel_down_sample(voxel_size=0.01)
 o3d.visualization.draw_geometries([downpcd])
-print("Done 2")
 
 # compute normals
 print("3 - Recompute the normal of the downsampled point cloud")
 downpcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
 o3d.visualization.draw_geometries([downpcd])
-print("Done 3")
 
 # orient normals
 print("4 - Orient computed normals")
 downpcd.orient_normals_towards_camera_location(camera_location=np.array([0., 0., 0.]))
 o3d.visualization.draw_geometries([downpcd])
-print("Done 4")
 
 # reconstruct via Poisson
 print("5 - Reconstruct the point cloud via Poisson algorythm")
 with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
     mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(downpcd, depth=9)
 o3d.visualization.draw_geometries([mesh])
-print("Done 5")
 
 # visualize low density areas
 print("6 - Visualize mesh density")
@@ -44,14 +39,12 @@ density_mesh.triangles = mesh.triangles
 density_mesh.triangle_normals = mesh.triangle_normals
 density_mesh.vertex_colors = o3d.utility.Vector3dVector(density_colors)
 o3d.visualization.draw_geometries([density_mesh])
-print("Done 6")
 
 # remove vertices with low density
 print("7 - Remove low density vertices")
 vertices_to_remove = densities < np.quantile(densities, 0.01)
 mesh.remove_vertices_by_mask(vertices_to_remove)
 o3d.visualization.draw_geometries([mesh])
-print("Done 7")
 
 # save
 o3d.io.write_triangle_mesh("/Users/veronika/Desktop/poisson_output_mesh.ply", mesh)
